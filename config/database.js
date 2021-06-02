2bab81cb7eebf123d11bfe0d77f253221ec99efa
Junior <juniormbaya243@gmail.com>
2021-06-02 13:17:16 +0200

    add cloudinary api

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.lmiia.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'junior_backend_portfolio2021'),
-        username: env('DATABASE_USERNAME', 'SupraAdmin'),
-        password: env('DATABASE_PASSWORD', 'Hope31'),
+        host: env("DATABASE_HOST"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
